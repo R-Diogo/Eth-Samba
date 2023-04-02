9233ff09670b133468b677c2a738c500945184d1
R-Diogo <dibuenor@gmail.com>
2023-04-01 21:01:35 -0300

    SLA

--- game.js
+++ game.js
@@ -30,6 +30,113 @@ function game(n,ans){
       i++;
     }
     else {
+    document.getElementById("myConnect").style.display = "block";
     output.textContent = (`Congrats, you guessed the number! Your score was ${(score - i/2).toFixed(1)}\n`);
+    score=10*(score-i/2);
     }
     }
+
+
+async function connect(){
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+        await provider.send("eth_requestAccounts", []);
+
+        const abi=[
+{
+    "inputs": [],
+    "name": "award",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+},
+{
+    "inputs": [
+        {
+            "internalType": "uint256",
+            "name": "points",
+            "type": "uint256"
+        }
+    ],
+    "name": "pay",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+},
+{
+    "inputs": [
+        {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+        }
+    ],
+    "name": "addressToPoints",
+    "outputs": [
+        {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [
+        {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+        }
+    ],
+    "name": "alreadyPlayed",
+    "outputs": [
+        {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [],
+    "name": "seeBlockElapsedTime",
+    "outputs": [
+        {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [
+        {
+            "internalType": "uint256",
+            "name": "index",
+            "type": "uint256"
+        }
+    ],
+    "name": "seeLeaderboard",
+    "outputs": [
+        {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+}
+]
+const contractaddress="0xF0797C150A96d2E6b97Aa0aEA937ab56a3Bb6C5f"
+const gameContract = new ethers.Contract(contractaddress, abi, provider);
+const signer = provider.getSigner()
+const res = await gameContract.connect(signer).pay(10);
+}
+
